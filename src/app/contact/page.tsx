13b5b9bbb6cb654c2d35ca5c3debd86d79425f7a
spencer-rafada <spencer.byui@gmail.com>
2023-12-27 18:25:31 -0800

    Added routes and pages with Metadata

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next'
+import React from 'react'
+
+export const metadata: Metadata = {
+  title: 'Contact',
+  description: 'Contact Page',
+}
+
+export default function Contact() {
+  return <div>Contact Page</div>
+}
---
+++ src/app/team/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next'
+import React from 'react'
+
+export const metadata: Metadata = {
+  title: 'Team',
+  description: 'Team Page',
+}
+
+export default function Team() {
+  return <div>Team Page</div>
+}
