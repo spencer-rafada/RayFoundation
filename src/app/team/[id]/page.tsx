fbdb6b143fa3c49b7511464671c52f155afabca8
spencer-rafada <spencer.byui@gmail.com>
2024-01-03 18:37:21 -0800

    added missing generateStaticParams

--- src/app/team/[id]/page.tsx
+++ src/app/team/[id]/page.tsx
@@ -19,6 +19,10 @@ Promise<Metadata> {
   }
 }
 
+export async function generateStaticParams() {
+  return [{ id: '1' }, { id: '2' }, { id: '3' }]
+}
+
 export default function TeamMember({ params }: { params: { id: string } }) {
   return <div>TeamMember {params.id}</div>
 }
