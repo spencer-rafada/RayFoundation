2f09b63582cdb8b65d147d9892de547a3bca53b8
spencer-rafada <spencer.byui@gmail.com>
2024-01-23 21:21:17 -0700

    added about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,16 @@ import React, { Suspense, lazy } from 'react'
 
 const HeroHeader = lazy(() => import('./(home)/HeroHeader'))
 const CTASection = lazy(() => import('./(home)/CTASection'))
+const About = lazy(() => import('./(home)/About'))
 
 export default function Home() {
   return (
     <>
       <Suspense fallback={<></>}>
         <HeroHeader />
+      </Suspense>
+      <Suspense fallback={<></>}>
+        <About />
       </Suspense>
       <Suspense fallback={<></>}>
         <CTASection />
