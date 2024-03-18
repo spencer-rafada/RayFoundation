8653d6c3ada09285033ee20b5a93d6f95e0955f6
spencer-rafada <spencer.byui@gmail.com>
2024-03-17 18:42:26 -0700

    refactored home page and about us page CTA Sections

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from 'next'
 import React from 'react'
 import OurStory from './components/OurStory'
 import VisionMission from './components/VisionMission'
+import CTASection from '../(home)/CTASection'
 
 export const metadata: Metadata = {
   title: 'About',
@@ -13,6 +14,10 @@ export default function About() {
     <>
       <OurStory />
       <VisionMission />
+      <CTASection
+        heading='Get to Know Us More'
+        subheading='We can tell you more about how your donations are put in to good use.'
+      />
     </>
   )
 }
