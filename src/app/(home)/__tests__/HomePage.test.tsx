d357b001ff0b654fe350e28ff9169a0bc4529c23
spencer-rafada <spencer.byui@gmail.com>
2024-03-17 17:38:47 -0700

    removed initial commit test

--- src/app/(home)/__tests__/HomePage.test.tsx
+++ src/app/(home)/__tests__/HomePage.test.tsx
@@ -4,11 +4,6 @@ import Home from '@/app/page'
 import '@testing-library/jest-dom'
 
 describe('Home Page', () => {
-  it('should render home page', () => {
-    render(<Home />)
-
-    expect(screen.getByText('Ray Foundation')).toBeInTheDocument()
-  })
   it('should render the hero header', async () => {
     const { getByText } = render(<Home />)
     const lazyElement = await waitFor(() =>
